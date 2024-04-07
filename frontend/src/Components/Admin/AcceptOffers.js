899a5bb9eeae6f374bcfc3960b5dabc96ecd9551
Atika Hamid <hafizaatikahamid965@gmail.com>
2024-04-07 13:19:09 +0500

    admin complete

--- frontend/src/Components/Admin/AcceptOffers.js
+++ frontend/src/Components/Admin/AcceptOffers.js
@@ -1,9 +1,144 @@
-import React from 'react'
+import React , { useState }from 'react'
+import DataTable from 'react-data-table-component';
 
 export default function AcceptOffers() {
+  const columns = [
+    {
+      name: 'Product',
+      selector: row => row.product,
+      maxWidth:"150px",
+      // minWidth:"100px",
+      sortable:true
+    },
+    {
+      name: 'Vendor ID',
+      selector: row => row.vendor_id,
+      maxWidth:"120px",
+      sortable:true
+    },
+    {
+      name: 'Customer ID',
+      selector: row => row.customer_id,
+      maxWidth:"120px",
+      sortable:true
+    },
+    {
+      name: 'Unit Price',
+      selector: row => row.uprice,
+      maxWidth:"10px"
+    },
+    {
+      name: 'quantity',
+      selector: row => row.quantity,
+      maxWidth:"10px"
+    },
+    {
+      name: 'Total Price',
+      selector: row => row.tprice,
+      maxWidth:"10px"
+    },
+    // {
+    //   name: 'Condition',
+    //   selector: row => row.condition,
+    //   maxWidth:"10px"
+    // },
+    // {
+    //   name: 'Category',
+    //   selector: row => row.category,
+    //   maxWidth:"15px"
+    // },
+    {
+      name: 'Offer Price',
+      selector: row => row.oprice,
+      maxWidth:"10px"
+    }
+  ]
+  const data = [
+    {
+      id: 1,
+      product: 'Car Brakes Honda 2016 Model',
+      vendor_id: 'khadija@gmail.com',
+      customer_id: 'khadija@gmail.com',
+      uprice: 4650000,
+      quantity: 34,
+      tprice:87487,
+      oprice: 80000, 
+    },
+    {
+      id: 2,
+      product: 'Car Brakes Honda 2016 Model',
+      vendor_id: 'khadija@gmail.com',
+      customer_id: 'khadija@gmail.com',
+      uprice: 4650000,
+      quantity: 34,
+      tprice:87487,
+      oprice: 80000, 
+    },
+    {
+      id: 3,
+      product: 'Car Brakes Honda 2016 Model',
+      vendor_id: 'khadija@gmail.com',
+      customer_id: 'khadija@gmail.com',
+      uprice: 4650000,
+      quantity: 34,
+      tprice:87487,
+      oprice: 80000, 
+    },
+    {
+      id: 4,
+      product: 'Car Brakes Honda 2016 Model',
+      vendor_id: 'khadija@gmail.com',
+      customer_id: 'khadija@gmail.com',
+      uprice: 4650000,
+      quantity: 34,
+      tprice:87487,
+      oprice: 80000, 
+    },
+    {
+      id: 5,
+      product: 'Car Brakes Honda 2016 Model',
+      vendor_id: 'khadija@gmail.com',
+      customer_id: 'khadija@gmail.com',
+      uprice: 4650000,
+      quantity: 34,
+      tprice:87487,
+      oprice: 80000, 
+    },
+    {
+      id: 6,
+      product: 'Car Brakes Honda 2016 Model',
+      vendor_id: 'khadija@gmail.com',
+      customer_id: 'khadija@gmail.com',
+      uprice: 4650000,
+      quantity: 34,
+      tprice:87487,
+      oprice: 80000, 
+    },
+  ]
+  const [records,setRecords]= useState(data);
+
+  function handleFilter(event){
+    const newData=data.filter(row=>{
+      return row.name.toLowerCase().includes(event.target.value.toLowerCase())
+    })
+    setRecords(newData);
+  }
   return (
-    <div>
-      <h1>accepted offers</h1>
+    <div className='categoryContainer'>
+    <div className="inner m-5 mt-3 p-5 mt-3 pt-2">
+      <h1>Accepted Offers</h1>
+    <div className=" pt-1 text-end"><label className='me-2 fs-6'>Search</label><input type="text" onChange={handleFilter} /></div>
+      <div className="tableContainer mt-3">
+        <DataTable
+         columns={columns}
+         data={records}
+         selectableRows
+         fixedHeader
+         fixedHeaderScrollHeight='300px'
+         pagination
+        ></DataTable>
+      </div>
     </div>
+  </div>
   )
 }
--- frontend/src/Components/Admin/DashboardA.js
+++ frontend/src/Components/Admin/DashboardA.js
@@ -1,9 +1,28 @@
 import React from 'react'
 
+import DashCard from '../user/DashCard'
+
 export default function DashboardA() {
   return (
-    <div>
-      <h1>dashboard</h1>
+    <div className='categoryContainer  dashboard_container'>
+      <div className="row d-flex inner_dashcard">
+        <DashCard name='Total Category' imgsource={require('../../assets/images/User/pending offers.jpg')} />
+        <DashCard name='Total Products' imgsource={require('../../assets/images/User/complte.jpg')} />
+        <DashCard name='Sold Products' imgsource={require('../../assets/images/User/cart.png')} />
+        <DashCard name='Total Users' imgsource={require('../../assets/images/User/cargo.png')} />
+      </div>
+      <div className="row d-flex inner_dashcard">
+        <DashCard name='Total Vendors' imgsource={require('../../assets/images/User/pending offers.jpg')} />
+        <DashCard name='Pending Offers' imgsource={require('../../assets/images/User/complte.jpg')} />
+        <DashCard name='Accepted Offers' imgsource={require('../../assets/images/User/cart.png')} />
+        <DashCard name='Rejected Offers' imgsource={require('../../assets/images/User/cargo.png')} />
+      </div>
+      <div className="row d-flex inner_dashcard">
+        <DashCard name='Complete Orders' imgsource={require('../../assets/images/User/pending offers.jpg')} />
+        <DashCard name='Pending Orders' imgsource={require('../../assets/images/User/complte.jpg')} />
+        <DashCard name='Cancel Orders' imgsource={require('../../assets/images/User/cart.png')} />
+        <DashCard name='Total Revenue' imgsource={require('../../assets/images/User/cargo.png')} />
+      </div>
     </div>
   )
 }
